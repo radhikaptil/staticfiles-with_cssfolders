79273b6a874fe7aa5f9b72ce954b8c608306d7a5
radhikaptil <patilradhika2001@gmail.com>
2023-08-06 23:10:01 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+# Create your views here.
+def first(request):
+    return render(request,'first.html')
+def second(request):
+    return render(request,'second.html')
+def third(request):
+    return render(request,'third.html')
+def fourth(request):
+    return render(request,'fourth.html')
+def fifth(request):
+    return render(request,'fifth.html')
+def sixth(request):
+    return render(request,'sixth.html')
+def seven(request):
+    return render(request,'seven.html')
+def eight(request):
+    return render(request,'eight.html')
+def nine(request):
+    return render(request,'nine.html')
+def ten(request):
+    return render(request,'ten.html')
+def eleven(request):
+    return render(request,'eleven.html')
+def twelve(request):
+    return render(request,'twelve.html')
+def thirteen(request):
+    return render(request,'thirteen.html')
+def fourteen(request):
+    return render(request,'fourteen.html')
+def fifteen(request):
+    return render(request,'fifteen.html')
+def sixteen(request):
+    return render(request,'sixteen.html')
+
+
+
+
+
